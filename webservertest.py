e716c87ddc738bfe15ec310a39af26d5db2de436
Ethan Chen <143357463+chenethan123@users.noreply.github.com>
2023-11-09 07:55:24 -0500

    Update webservertest.py

--- webservertest.py
+++ webservertest.py
@@ -7,7 +7,34 @@ kit = MotorKit(0x40)
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return '''
+    <html>
+    <body>
+        <h1>Robot Control</h1>
+        <button onclick="sendCommand('forward')">Forward</button>
+        <button onclick="sendCommand('backward')">Backward</button>
+        <button onclick="sendCommand('left')">Left</button>
+        <button onclick="sendCommand('right')">Right</button>
+        <button onclick="sendCommand('stop')">Stop</button>
 
+        <script>
+        function sendCommand(command) {
+            fetch('/control', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 'command': command })
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+        }
+        </script>
+    </body>
+    </html>
+    '''
 
 @app.route('/move', methods=['POST'])
 def control():
