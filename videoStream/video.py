de291bd3308eb3d59f05c4692de885ca40e73c22
Ethan Chen <143357463+chenethan123@users.noreply.github.com>
2024-01-11 07:59:13 -0500

    Update video.py

--- videoStream/video.py
+++ videoStream/video.py
@@ -47,5 +47,5 @@ def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=4444, debug=True)
